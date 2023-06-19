c51404b31625b79a7f3574d3d12ab90ca706d842
dysontree <135720784+dysontree@users.noreply.github.com>
2023-06-19 18:36:40 +0300

    Update change.groovy

--- change.groovy
+++ change.groovy
@@ -1,6 +1,6 @@
 def git() {
 
 git branch: 'main', url: 'https://github.com/elestopadov/jenkins-course-example.git'
-#git branch: 'main', url: 'https://github.com/dysontree/jenkins-example-app.git'
+//git branch: 'main', url: 'https://github.com/dysontree/jenkins-example-app.git'
   
 }
